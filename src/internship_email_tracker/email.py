8b91d9c4686dc60f525faaa47a3340e94e8287c9
Arjun Kishore <arjukishore2017@outlook.com>
2026-09-02 18:08:36 +0100

    Email model

---
+++ src/internship_email_tracker/email.py
@@ -0,0 +1,12 @@
+class Email:
+    def __init__(self, company, subject, date, stage):
+        self.company = company
+        self.subject = subject
+        self.date = date
+        self.stage = stage
+    
+
+if __name__ == "__main__":
+    email1 = Email("Amazon", "Online assessment invitation", "Sep 2", "Assessment")
+    print(email1.company)
+    print(email1.subject)
