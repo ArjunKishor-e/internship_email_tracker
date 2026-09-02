6f52c637c7378c0ba10cfed86eaf2797b0e3f604
Arjun Kishore <arjukishore2017@outlook.com>
2026-09-02 19:36:42 +0100

    Sample emails/Dashboard Render

--- src/internship_email_tracker/app.py
+++ src/internship_email_tracker/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from internship_email_tracker.sample_emails import sample_emails
 
 app = FastAPI()
 
@@ -7,4 +8,4 @@ templates = Jinja2Templates(directory="src/internship_email_tracker/templates")
 
 @app.get("/")
 def home(request: Request):
-    return templates.TemplateResponse(request, "index.html", {})
+    return templates.TemplateResponse(request, "index.html", {"emails": sample_emails})
---
+++ src/internship_email_tracker/sample_emails.py
@@ -0,0 +1,8 @@
+from internship_email_tracker.email import Email
+
+sample_emails = [
+    Email("Amazon", "Assessment Centre", "Sep 2", "AC"),
+    Email("Microsoft", "OA Invitation", "Oct 2", "Assessment"),
+    Email("Google", "OA Invitation", "Aug 2", "Assessment"),
+    Email("Meta", "Interview", "Feb 2", "Interview"),
+]
