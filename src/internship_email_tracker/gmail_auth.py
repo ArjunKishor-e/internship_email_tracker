e4142f39d8e359fb09163711594141b2697fc02b
Arjun Kishore <arjukishore2017@outlook.com>
2026-09-03 22:51:34 +0100

    Add Gmail Authentication

---
+++ src/internship_email_tracker/gmail_auth.py
@@ -0,0 +1,25 @@
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ["https://www.googleapis.com/auth/gmail.readonly"]
+
+def get_gmail_credentials():
+    creds = None
+
+    if os.path.exists("credentials/token.json"):
+        creds = Credentials.from_authorized_user_file("credentials/token.json", SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials/client_secret.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+
+        with open("credentials/token.json", "w") as token:
+            token.write(creds.to_json())
+
+    return creds
