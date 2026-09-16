de1789caf1e61eb6ae4994442a8c8714e7392a91
Arjun Kishore <arjukishore2017@outlook.com>
2026-09-16 22:33:48 +0100

    backend docker setup working

--- src/internship_email_tracker/gmail_auth.py
+++ src/internship_email_tracker/gmail_auth.py
@@ -12,8 +12,11 @@ def get_gmail_credentials():
         creds = Credentials.from_authorized_user_file("credentials/token.json", SCOPES)
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
+            try:
+                creds.refresh(Request())
+            except Exception:
+                creds = None
+        if not creds:
             flow = InstalledAppFlow.from_client_secrets_file(
                 "credentials/client_secret.json", SCOPES
             )
